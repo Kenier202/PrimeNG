6337e5a9d4d346be24d74ded94d6c402e6e42292
kenier <kenier202@gmail.com>
2024-10-29 15:08:15 -0500

    feat(PrimeNG): import primeNG modules

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,6 +1,11 @@
-import { NgModule } from '@angular/core';
-import { MenuModule } from 'primeng/menu';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+import { NgModule } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+
 
 @NgModule({
   declarations: [],
@@ -8,8 +13,11 @@ import { MenubarModule } from 'primeng/menubar';
 
   ],
   exports: [
-    MenuModule,
-    MenubarModule
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule,
   ]
 })
 export class PrimeNgModule { }
